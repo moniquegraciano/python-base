498ee360dac326209fc9098b4a8b75db2efeec37
Monique Graciano <moniqueograciano@gmail.com>
2025-01-09 11:30:53 -0300

    Aula 2

--- hello.py
+++ hello.py
@@ -1,3 +1,22 @@
-print("Hello, World!")
+#!/usr/bin/env python3
+
+
+"""Hello World multi linguas.
+"""
+import os
+
+__version__="0.0.1"
+__author__="Monique Graciano"
+__license__="Unlicense"
+
+current_language=os.getenv("LANG")[:5]
+msg="Hello, World!"
+
+if current_language=="pt_BR":
+    msg="Olá, Mundo!"
+elif current_language=="it_IT":
+    msg="Ciao, Mondo!"
+
+print(msg)
 
 
